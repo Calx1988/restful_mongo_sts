dcaaff936cb634c04c2a37d2283f24b1ffafddf5
Calx1988 <cassioalexsander@gmail.com>
2022-05-10 18:49:43 -0300

    Created User Data Transfer Object

--- src/main/java/restful_mongo_sts/resources/UserResource.java
+++ src/main/java/restful_mongo_sts/resources/UserResource.java
@@ -1,7 +1,7 @@
 package restful_mongo_sts.resources;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import restful_mongo_sts.domain.User;
+import restful_mongo_sts.dto.UserDTO;
 import restful_mongo_sts.services.UserService;
 
 @RestController
@@ -19,9 +20,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> listUsers = service.findAll();
-		return ResponseEntity.ok().body(listUsers);
+		List<UserDTO> listUsersDto = listUsers.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listUsersDto);
 	}
 	
 }
