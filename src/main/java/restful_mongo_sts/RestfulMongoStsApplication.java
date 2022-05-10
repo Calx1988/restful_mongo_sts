8aa2f0a2bb8d162236cd95dff807a27e2d796198
Calx1988 <cassioalexsander@gmail.com>
2022-05-10 13:17:04 -0300

    Creating Project

---
+++ src/main/java/restful_mongo_sts/RestfulMongoStsApplication.java
@@ -0,0 +1,13 @@
+package restful_mongo_sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulMongoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulMongoStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restful_mongo_sts/RestfulMongoStsApplicationTests.java
@@ -0,0 +1,13 @@
+package restful_mongo_sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulMongoStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
