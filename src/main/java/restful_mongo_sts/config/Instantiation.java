ffa8d3518242c9e30154e2558e1fd7243d325693
Calx1988 <cassioalexsander@gmail.com>
2022-05-11 16:39:55 -0300

    Linked posts to user

--- src/main/java/restful_mongo_sts/config/Instantiation.java
+++ src/main/java/restful_mongo_sts/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import restful_mongo_sts.domain.Post;
 import restful_mongo_sts.domain.User;
+import restful_mongo_sts.dto.AuthorDTO;
 import restful_mongo_sts.repositories.PostRepository;
 import restful_mongo_sts.repositories.UserRepository;
 
@@ -33,12 +34,18 @@ public class Instantiation implements CommandLineRunner{
 		User o2 = new User(null, "Olga Nikolaevna", "olganiko@outlook.com");
 		User o3 = new User(null, "Tatiana Romanov", "tatiromanov@hotmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("02/05/2022"), "Partiu!", "Viajando para a Suíça. Abraços!", o1);
-		Post post2 = new Post(null, sdf.parse("10/05/2022"), "Bom dia!", "Acordei feliz hoje! Visto aprovado!", o1);
-		Post post3 = new Post(null, sdf.parse("07/05/2022"), "Calorão!", "Que venha o inverno logo!", o3);
-		
 		userRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		Post post1 = new Post(null, sdf.parse("02/05/2022"), "Partiu!", "Viajando para a Suíça. Abraços!", new AuthorDTO(o1));
+		Post post2 = new Post(null, sdf.parse("10/05/2022"), "Bom dia!", "Acordei feliz hoje! Visto aprovado!", new AuthorDTO(o1));
+		Post post3 = new Post(null, sdf.parse("07/05/2022"), "Calorão!", "Que venha o inverno logo!", new AuthorDTO(o3));
+		
 		postReposiroty.saveAll(Arrays.asList(post1, post2, post3));
+		
+		o1.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(o1);
+		o3.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(o3);
 				
 	}
 
