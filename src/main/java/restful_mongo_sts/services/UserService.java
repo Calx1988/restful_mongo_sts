01d331492a8cf7b9a2edb5d84b1a2f46e1c3aeee
Calx1988 <cassioalexsander@gmail.com>
2022-05-11 12:51:07 -0300

    Created user insert

--- src/main/java/restful_mongo_sts/services/UserService.java
+++ src/main/java/restful_mongo_sts/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import restful_mongo_sts.domain.User;
+import restful_mongo_sts.dto.UserDTO;
 import restful_mongo_sts.repositories.UserRepository;
 import restful_mongo_sts.services.exceptions.ObjNotFoundException;
 
@@ -23,6 +24,13 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		
 			return user.orElseThrow(() -> new ObjNotFoundException("There's no user with this ID."));
+	}
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
