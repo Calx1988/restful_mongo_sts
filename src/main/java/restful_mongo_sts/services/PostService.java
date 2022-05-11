977e474b7af53b8c0456beaba42d9c105d3cf855
Calx1988 <cassioalexsander@gmail.com>
2022-05-11 16:58:02 -0300

    Find post by id

---
+++ src/main/java/restful_mongo_sts/services/PostService.java
@@ -0,0 +1,22 @@
+package restful_mongo_sts.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import restful_mongo_sts.domain.Post;
+import restful_mongo_sts.repositories.PostRepository;
+import restful_mongo_sts.services.exceptions.ObjNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id){
+		Optional<Post> post = repository.findById(id);
+		
+			return post.orElseThrow(() -> new ObjNotFoundException("There's no user with this ID."));
+	}
+}
